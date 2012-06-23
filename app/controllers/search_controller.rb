2158e0431a93ba3bceb4fe7bcc28c014f701ca81
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-06-22 20:55:03 -0500

    Create Search Page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
-
+  before_filter :set_static_vars
+  
 	def skills
 		@popup = params[:popup] || 'false'
     @search_string = params[:search_string]
@@ -10,5 +11,11 @@ class SearchController < ApplicationController
     @search_string = params[:search_string]
     @results = User.search(@search_string)
 	end
+
+	private
+    def set_static_vars
+      @page = (params[:page] || 1).to_i
+      @per_page = 10
+    end
 	
 end
