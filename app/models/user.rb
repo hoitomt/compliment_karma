e0b14040050f0fd63075b2617ef19425ef224064
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-07-02 01:09:03 -0500

    Fix Seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
                   :first_name, :middle_name, :last_name, :domain, :job_title,
                   :account_status_id, :account_status, :address_line_1, :address_line_2, 
                   :city, :state_cd, :zip_cd, :profile_url, :profile_headline, 
-                  :industry_id
+                  :industry_id, :company_id
   
   has_attached_file  :photo, 
                      :styles => { :mini => "40x40>",
