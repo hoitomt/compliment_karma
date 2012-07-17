2006e6dcf0ee4b0c5120a1210deeeb16243e1616
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-07-17 14:28:18 -0500

    Update footer

--- app/assets/javascripts/footer_resize.js
+++ app/assets/javascripts/footer_resize.js
@@ -1,38 +1,20 @@
 var FooterResize = {
 	init: function() {
 		var viewportHeight = $.waypoints('viewportHeight');
+		var headerHeight = $('#header').height();
+		var mainContainerHeight = $('#main-container').height();
 		var footerHeight = $('#footer').height();
-		var footerTop = $('#footer').offset().top;
-		var footerBottom = $('#footer').offset().top + footerHeight;
-		var mainContainerBottom = $('#main-container').offset().top + $('#main-container').height();
-		// console.log("Footer Bottom: " + footerBottom + 
-		// 	'| View Port Height: ' +  viewportHeight +
-		// 	'| Main Container Bottom: ' + mainContainerBottom);
-		if(footerBottom < mainContainerBottom) {
-			// The page is missing the footer
-			return;
-		}
-		if(viewportHeight > footerBottom) {
-			var delta = viewportHeight - footerBottom;
-			var mainContainerHeight = $('#main-container').height()
+		var contentHeight = headerHeight + mainContainerHeight + footerHeight;
+		var delta = viewportHeight - contentHeight;
+		console.log("Header Height: " + headerHeight + 
+								"| Main Container Height: " + mainContainerHeight +
+								"| Footer Height: " + footerHeight + 
+								"| View port height: " + viewportHeight);
+		if(delta > 0) {
 			$('#expansion-div').height(delta);
+		} else {
+			$('#expansion-div').height(0);
 		}
-
-		// var documentBottom = $(document).height();
-		// // console.log(footerBottom + '|' + documentBottom);
-		// if(footerBottom && footerBottom < documentBottom && $('#footer').is(":visible")) {
-		// 	var mainHeight = $('#main-container').height();
-		// 	if(mainHeight && mainHeight > 0) {
-		// 		$('#main-container').height(mainHeight + (documentBottom-footerBottom));
-		// 	} else {
-		// 		// var expansionHeight = $('#expansion-div').height();
-		// 		var delta = documentBottom - footerBottom;
-		// 		// var newHeight = expansionHeight + delta;
-		// 		// console.log(expansionHeight + '|' + newHeight);
-		// 		$('#expansion-div').height(delta);
-		// 		// $('#expansion-div').height(newHeight);
-		// 	}
-		// }
 	}
 	
 }
