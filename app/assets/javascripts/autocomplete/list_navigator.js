43803bc9c949622e838e5ab7e4b4806f856ee7ff
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-07-18 10:18:17 -0500

    Update Search

--- app/assets/javascripts/autocomplete/list_navigator.js
+++ app/assets/javascripts/autocomplete/list_navigator.js
@@ -32,31 +32,36 @@ var ListNavigator = {
 				ListNavigator.highlightElement(list);
 			} else if(e.keyCode == 13) { // enter key
 				var listElement = list[ListNavigator.index];
-				// Make sure we only care about enter for the appropriate results
-				var skillAutoCompleteContainer = $(listElement).parents('#skill-auto-complete');
-				var complimentReceiverContainer = $(listElement).parents('#compliment-receiver-results');
-				var siteSearchContainer = $(listElement).parents('#site-search-results');
-				if(siteSearchContainer.length > 0) {
-					ListNavigator.retrieveUserAndClose(listElement);
-				} else if (skillAutoCompleteContainer.length > 0) {
-					ListNavigator.setInputAndClose(listElement);
-				} else if (complimentReceiverContainer.length > 0) {
-					ListNavigator.setInputAndClose(listElement);
-				}
+				ListNavigator.handleSelect(listElement);
 				return false;
 			}
 		});
 		$('li.register-me').off('click');
 		$('li.register-me').click(function() {
-			// console.log("clicky register");
-			ListNavigator.setInputAndClose($(this)[0])
-			ComplimentUI.validateSenderNotReceiver();
+			listElement = this;
+			ListNavigator.handleSelect(listElement);
 		})
 		$(list).on({
 			hover: function() {
 				ListNavigator.unhighlightList(this);
 			}
 		});
+	},
+	handleSelect: function(listElement) {
+		// Make sure we only care about enter for the appropriate results
+		var skillAutoCompleteContainer = $(listElement).parents('#skill-auto-complete');
+		var complimentReceiverContainer = $(listElement).parents('#compliment-receiver-results');
+		var siteSearchContainer = $(listElement).parents('#site-search-results');
+		if(siteSearchContainer.length > 0) {
+			ListNavigator.retrieveUserAndClose(listElement);
+		} else if (skillAutoCompleteContainer.length > 0) {
+			ListNavigator.setInputAndClose(listElement);
+		} else if (complimentReceiverContainer.length > 0) {
+			ListNavigator.setInputAndClose(listElement);
+			ComplimentUI.validateSenderNotReceiver();
+		} else {
+			ComplimentUI.validateSenderNotReceiver();
+		}
 	},
 	setInputAndClose: function(listElement) {
 		var inputValue = $(listElement).find('.value-for-input-field');
