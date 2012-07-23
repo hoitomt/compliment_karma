fbfc0aa0876cb827f452e4409af127b785453598
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-07-23 11:01:02 -0500

    fix footer resize

--- app/assets/javascripts/footer_resize.js
+++ app/assets/javascripts/footer_resize.js
@@ -2,15 +2,18 @@ var FooterResize = {
 	init: function() {
 		var viewportHeight = $.waypoints('viewportHeight');
 		var headerHeight = $('#header').height();
-		var mainContainerHeight = $('#main-container').height();
+		var mainContainerHeight = $('#main-container').height() + 20; //padding
 		var footerHeight = $('#footer').height();
-		var footerVisible = $('footer').is(':visibile');
+		var footerVisible = $('#footer').length > 0;
 		var contentHeight = headerHeight + mainContainerHeight + footerHeight;
 		var delta = viewportHeight - contentHeight;
 		console.log("Header Height: " + headerHeight + 
 								"| Main Container Height: " + mainContainerHeight +
 								"| Footer Height: " + footerHeight + 
-								"| View port height: " + viewportHeight);
+								"| Content Height: " + contentHeight +
+								"| View port height: " + viewportHeight +
+								"| Delta: " + delta +
+								"| Footer Visible: " + footerVisible);
 		if(delta > 0 && footerVisible) {
 			$('#expansion-div').height(delta);
 		} else {
