d9db544aafde19bfb695b1fd5ca22a646b14c9a3
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-07-24 13:50:29 -0500

    Fix unconfirmed user account switching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
                                            :professional_profile, :social_profile, 
                                            :achievements, :contacts, :employees, 
                                            :rewards, :settings, :my_updates, :switch_accounts ]
-  before_filter :get_confirmation_status, :except => [:new, :create]
+  before_filter :get_confirmation_status, :except => [:new, :create, :switch_accounts]
   before_filter :hide_unconfirmed_user
   before_filter :set_static_vars
   before_filter :set_compliment_panel, 
