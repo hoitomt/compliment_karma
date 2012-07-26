d4e345e3fcc1aaad8631a1df584ecd5ae052077a
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-07-26 13:37:21 -0500

    Fix profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   
   has_attached_file  :photo, 
                      :styles => { :mini => "40x40>",
-                                  :thumb => "60x60>",
+                                  :thumb => "60>x60",
                                   :small => "180>x180",
                                   :profile => "200>x200",
                                   :medium => "300x300>" },
