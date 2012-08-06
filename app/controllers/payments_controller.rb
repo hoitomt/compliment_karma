30bbbbee3552d6b65042e9dcdb03a0ece2ad2b4a
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-08-05 22:25:26 -0500

    Fix null cart at checkout

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,10 +3,15 @@ class PaymentsController < ApplicationController
 	before_filter :get_user
 
 	def new
-		@cart_total = session[:cart].inject(0){ |sum, r_id| sum + Reward.find(r_id).value }
-		@service_fee = @cart_total.to_f * 0.08 + 1
-		@order_total = @cart_total.to_f + @service_fee.to_f
-		@payment = Payment.new
+		if session[:cart].blank?
+			flash[:error] = "Your cart does not contain any items"
+			redirect_to :back
+		else
+			@cart_total = session[:cart].inject(0){ |sum, r_id| sum + Reward.find(r_id).value }
+			@service_fee = @cart_total.to_f * 0.08 + 1
+			@order_total = @cart_total.to_f + @service_fee.to_f
+			@payment = Payment.new
+		end
 	end
 
 	def create
