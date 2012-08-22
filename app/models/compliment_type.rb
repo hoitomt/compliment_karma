bdd4b060d1ab3065e503b7f50351194400246225
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-08-22 01:20:01 -0500

    Update Compliment type and remove tooltips

--- app/models/compliment_type.rb
+++ app/models/compliment_type.rb
@@ -2,22 +2,32 @@ class ComplimentType < ActiveRecord::Base
   has_many :compliments
   
   validates_uniqueness_of :name
-  default_scope :order => 'id ASC'
+  default_scope :order => "id ASC"
   
+# Change the Compliment Type drop down options to following (Preserve the upper/lowercase, exactly as i entered below):
+# - from my PROFESSIONAL to receiver’s PROFESSIONAL profile
+# - from my PROFESSIONAL to receiver’s SOCIAL profile
+# - from my SOCIAL to receiver’s PROFESSIONAL profile
+# - from my SOCIAL to receiver’s SOCIAL profile
+
   def self.PROFESSIONAL_TO_PROFESSIONAL
-    find_by_name('Professional to Professional')
+    find_by_name("Professional to Professional") || 
+    find_by_name("from my PROFESSIONAL to receivers PROFESSIONAL profile")
   end
   
   def self.PROFESSIONAL_TO_PERSONAL
-    find_by_name('Professional to Personal')
+    find_by_name("Professional to Personal") || 
+    find_by_name("from my PROFESSIONAL to receivers SOCIAL profile")
   end
   
   def self.PERSONAL_TO_PROFESSIONAL
-    find_by_name('Personal to Professional')
+    find_by_name("Personal to Professional") || 
+    find_by_name("from my SOCIAL to receivers PROFESSIONAL profile")
   end
   
   def self.PERSONAL_TO_PERSONAL
-    find_by_name('Personal to Personal')
+    find_by_name("Personal to Personal") || 
+    find_by_name("from my SOCIAL to receivers SOCIAL profile")
   end
 
   def self.is_professional?(compliment_type_id)
