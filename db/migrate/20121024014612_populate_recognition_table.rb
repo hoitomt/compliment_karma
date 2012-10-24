d5a7f8b70b6adfdebb5e71af1a5fc4fd089be0be
Mike Hoitomt <mike.hoitomt@gmail.com>
2012-10-23 21:51:31 -0500

    Add Migration to create recognition objects

--- db/migrate/20121024014612_populate_recognition_table.rb
+++ db/migrate/20121024014612_populate_recognition_table.rb
@@ -1,7 +1,20 @@
 class PopulateRecognitionTable < ActiveRecord::Migration
-  def up
+  def change
+    p "Create recognition from compliment"
+    Compliment.all.each do |c|
+      Recognition.create_from_compliment(c)
+    end
+
+    p "Create recognition from reward"
+    Reward.all.each do |r|
+      Recognition.create_from_reward(r)
+    end
+
+    p "Create recognition from accomplishment"
+    Accomplishment.all.each do |a|
+      Recognition.create_from_accomplishment(a)
+    end
+
   end
 
-  def down
-  end
 end
