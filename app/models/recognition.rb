ee3e5bd5e1383a1e58d6e1a62cd9e45942116763
Mike Hoitomt <mike.hoitomt@gmail.com>
2012-10-25 22:43:35 -0500

    Fix reward

--- app/models/recognition.rb
+++ app/models/recognition.rb
@@ -1,9 +1,7 @@
 class Recognition < ActiveRecord::Base
   belongs_to :compliment, :foreign_key => :recognition_id
-  belongs_to :reward, :foreign_key => :recognition_id,
-                      :conditions => {:recognition_type_id => RecognitionType.REWARD.id}
-  belongs_to :user_accomplishment, :foreign_key => :recognition_id,
-                      :conditions => {:recognition_type_id => RecognitionType.ACCOMPLISHMENT.id}
+  belongs_to :reward, :foreign_key => :recognition_id
+  belongs_to :user_accomplishment, :foreign_key => :recognition_id
 
   validates_presence_of :recognition_id, :recognition_type_id
   validates_uniqueness_of :recognition_id, :scope => [:recognition_type_id]
