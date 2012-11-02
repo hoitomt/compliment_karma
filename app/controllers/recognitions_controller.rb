eedc14057729ee7e39c223e072c0758e97f04662
Mike Hoitomt <mike.hoitomt@gmail.com>
2012-11-01 22:32:25 -0500

    Update social description with .com

--- app/controllers/recognitions_controller.rb
+++ app/controllers/recognitions_controller.rb
@@ -20,7 +20,7 @@ class RecognitionsController < ApplicationController
   def set_meta_tags
     @og_meta_url = fb_like_url(@recognition)
     @og_meta_title = og_meta_description(@recognition)
-    @og_meta_description = "ComplimentKarma" #og_meta_description(@recognition)
+    @og_meta_description = "ComplimentKarma.com"
     @meta_description = og_meta_description(@recognition)
     @og_meta_image = og_meta_image(@recognition)
   end
